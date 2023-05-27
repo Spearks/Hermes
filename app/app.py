1262a744a5eb23a30597e80e3449584f3d891213
Spearks <otavio.canuto.m@gmail.com>
2023-05-27 12:04:06 -0300

    remove ready() on app

--- app/app.py
+++ app/app.py
@@ -5,9 +5,3 @@ class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
 
-    def ready(self):
-
-        from app.models import DeviceModel
-        
-        for Device in DeviceModel.objects.all():
-            print(Device)
